60c24a7bdaea0a16b1d56c10b5a2c94678c9afb0
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-02-15 20:19:58 +0530

    commit

--- src/services/Authcontext.js
+++ src/services/Authcontext.js
@@ -2,24 +2,22 @@ import React, { createContext, useContext, useReducer, useEffect } from "react";
 
 const initialState = {
   user: null,
-  loading: true, // Add loading state to track asynchronous loading
+  loading: true,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(action.payload));
       return {
         ...state,
         user: action.payload,
-        loading: false, // Set loading to false after user data is set
+        loading: false,
       };
     case "LOGOUT":
-      localStorage.removeItem("user");
       return {
         ...state,
         user: null,
-        loading: false, // Set loading to false after logout
+        loading: false,
       };
     default:
       return state;
@@ -32,15 +30,16 @@ export const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    // Check if user data exists in localStorage
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    if (storedUser) {
-      // If user data exists, dispatch LOGIN action with stored user data
-      dispatch({ type: "LOGIN", payload: storedUser });
-    } else {
-      // If no user data in localStorage, set loading to false
-      dispatch({ type: "SET_LOADING_FALSE" });
-    }
+    const fetchUser = async () => {
+      const storedUser = await JSON.parse(localStorage.getItem("user"));
+      if (storedUser) {
+        dispatch({ type: "LOGIN", payload: storedUser });
+      } else {
+        dispatch({ type: "SET_LOADING_FALSE" });
+      }
+    };
+
+    fetchUser();
   }, []);
 
   const login = (userData) => {
